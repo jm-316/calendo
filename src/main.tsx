8d1ae9f721e3bb0af8625be22178081108f7c172
jm-316 <yoyo3302@gmail.com>
2024-09-01 14:41:03 +0900

    초기 설정

---
+++ src/main.tsx
@@ -0,0 +1,40 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import Dashboard from "./pages/dashboard/index.tsx";
+import Todos from "./pages/todos/index.tsx";
+import Calendars from "./pages/calendars/index.tsx";
+import CalendarNewPage from "./pages/calendars/new.tsx";
+import Login from "./pages/login/index.tsx";
+import NotFound from "./pages/notFound/index.tsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      {
+        path: "todos",
+        element: <Todos />,
+      },
+    ],
+  },
+  {
+    path: "calendars",
+    children: [
+      { path: "/calendars", element: <Calendars /> },
+      { path: "/calendars/new", element: <CalendarNewPage /> },
+    ],
+  },
+  { path: "/login", element: <Login /> },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,3 @@
+export default function Login() {
+  return <div className="text-blue-600">Login</div>;
+}
