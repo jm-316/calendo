b4b854fce831773cc6ef915f76cee20b3a4a8a1e
jm-316 <yoyo3302@gmail.com>
2024-09-02 16:05:01 +0900

    dark모드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,32 @@
 import { Outlet } from "react-router";
+import { useEffect, useState } from "react";
+import Header from "./components/Header";
 
 function App() {
+  const [dark, setDark] = useState(() => {
+    return localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ? true
+      : false;
+  });
+
+  useEffect(() => {
+    if (dark) {
+      document.documentElement.classList.add("dark");
+      localStorage.theme = "dark";
+    } else {
+      document.documentElement.classList.remove("dark");
+      localStorage.theme = "light";
+    }
+  }, [dark]);
+
+  const darkSetButton = () => {
+    setDark((prevState) => !prevState);
+  };
   return (
     <>
+      <Header dark={dark} darkSetButton={darkSetButton} />
       <Outlet />
     </>
   );
