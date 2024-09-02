3d9f8b94e4312d3e9bea0dffea66bd733ea8dc99
jm-316 <yoyo3302@gmail.com>
2024-09-02 16:04:39 +0900

    Header 구현

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { MdDarkMode } from "react-icons/md";
+import { IoIosSearch } from "react-icons/io";
+
+interface HeaderProps {
+  dark: boolean;
+  darkSetButton: () => void;
+}
+
+export default function Header({ dark, darkSetButton }: HeaderProps) {
+  return (
+    <header className={`bg-white border-b-2 ${dark ? "dark" : ""}`}>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8">
+        <div className="hidden lg:block lg:flex-1">
+          <Link to="/" className="-m-1.5 p-1.5">
+            <span className="text-xl italic font-bold text-purple-500">
+              CalenDo
+            </span>
+          </Link>
+        </div>
+
+        <div className="flex items-center gap-x-3 w-7/12 relative lg:flex md:mx-auto">
+          <div className="absolute left-2 top-[13px] ">
+            <IoIosSearch />
+          </div>
+          <input
+            className={`block w-full rounded-md border-2 py-1.5 pl-7 pr-20 placeholder:text-gray-400 lg:placeholder:text-lg ${
+              dark ? "dark" : ""
+            }`}
+            placeholder="search"
+          />
+          <MdDarkMode
+            onClick={darkSetButton}
+            className="h-9 w-9 lg:w-9 lg:h-9 text-yellow-400"
+          />
+        </div>
+        <div className="flex items-center justify-end lg:flex-1 px-8">
+          <button
+            className={`font-semibold leading-6 text-gray-900  ${
+              dark ? "dark" : ""
+            }`}>
+            Log in <span aria-hidden="true">&rarr;</span>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
