6813889bc2d38428a82c3404ad3fd7f73c296353
jm-316 <yoyo3302@gmail.com>
2024-09-03 21:04:27 +0900

    선택 효과 적용

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -2,8 +2,17 @@ import { Link } from "react-router-dom";
 import { TiHomeOutline } from "react-icons/ti";
 import { FaRegCalendarAlt } from "react-icons/fa";
 import { LuListTodo } from "react-icons/lu";
+import { useState } from "react";
+
+type NavType = "Dashboard" | "Calendar" | "Todo";
 
 export default function Sidebar() {
+  const [selected, setSelected] = useState<NavType>("Dashboard");
+
+  const handleSelect = (name: NavType) => {
+    setSelected(name);
+  };
+
   return (
     <div className="flex w-20 lg:w-64">
       <div className="flex pl-3 pr-3">
@@ -17,19 +26,32 @@ export default function Sidebar() {
           </div>
           <ul className="flex flex-1 flex-col ml-1.5">
             <li className="mb-8">
-              <Link to="/" className="nav__list select">
+              <Link
+                to="/"
+                className={`nav__list ${
+                  selected === "Dashboard" ? "select" : ""
+                } `}
+                onClick={() => handleSelect("Dashboard")}>
                 <TiHomeOutline className="w-6 h-6 shrink-0" />
                 <span className="hidden lg:block lg:w-36">Dashboard</span>
               </Link>
             </li>
             <li className="mb-8">
-              <Link to="/calendars" className="nav__list">
+              <Link
+                to="/calendars"
+                className={`nav__list ${
+                  selected === "Calendar" ? "select" : ""
+                } `}
+                onClick={() => handleSelect("Calendar")}>
                 <FaRegCalendarAlt className="w-6 h-6 shrink-0" />
                 <span className="hidden lg:block w-40">Calendar</span>
               </Link>
             </li>
             <li>
-              <Link to="/todos" className="nav__list">
+              <Link
+                to="/todos"
+                className={`nav__list ${selected === "Todo" ? "select" : ""} `}
+                onClick={() => handleSelect("Todo")}>
                 <LuListTodo className="w-6 h-6 shrink-0" />
                 <span className="hidden lg:block w-40">To do</span>
               </Link>
