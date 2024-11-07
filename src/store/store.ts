419f9d8c58df50f133fdca59ecd93a31640a7d8d
jm-316 <yoyo3302@gmail.com>
2024-11-07 17:40:11 +0900

    redux-persist store 작성

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,10 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistReducer, persistStore } from "redux-persist";
+import sessionStorage from "redux-persist/lib/storage/session";
 import schedulerSlice from "../slices/schedulerSlice";
 
+const persistConfig = {
+  key: "root",
+  storage: sessionStorage,
+  whitelist: ["scheduler"],
+};
+
+const rootReducer = combineReducers({
+  scheduler: schedulerSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-  reducer: {
-    scheduler: schedulerSlice,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export const persistor = persistStore(store);
