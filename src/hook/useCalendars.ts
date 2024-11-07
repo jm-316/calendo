ab5258f1d9d969b954d905a037e11fbb137a15c1
jm-316 <yoyo3302@gmail.com>
2024-11-07 17:43:07 +0900

    useCalendars newEvent 추가

--- src/hook/useCalendars.ts
+++ src/hook/useCalendars.ts
@@ -1,10 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useUser } from "./useUser";
 import { CalendarType } from "../interface";
-import { getCalendars } from "../services/apiCalendar";
+import { addEvent, getCalendars } from "../services/apiCalendar";
 
 export function useCalendars() {
   const { user } = useUser();
+
+  const queryClient = useQueryClient();
 
   const calendarsQuery = useQuery<CalendarType[]>({
     queryKey: ["calendars", user?.id],
@@ -13,5 +15,24 @@ export function useCalendars() {
     },
   });
 
-  return { calendars: calendarsQuery.data };
+  const newEvent = useMutation({
+    mutationFn: (event: {
+      title: string;
+      startDate: string;
+      startTime?: string;
+      endDate?: string;
+      endTime?: string;
+      content: string;
+      color: string;
+      userId: string;
+    }) => addEvent(event),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["calendars"] });
+    },
+    onError: (error) => {
+      console.error("Failed to add calendars:", error);
+    },
+  });
+
+  return { calendars: calendarsQuery.data, newEvent };
 }
