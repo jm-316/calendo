630d7f016a0abc4ace8394ab225cc73fdf2e7f82
jm-316 <yoyo3302@gmail.com>
2024-11-07 17:41:45 +0900

    colors 와 week 코드 분리

---
+++ src/utils/constants.ts
@@ -0,0 +1,13 @@
+import { ColorsType } from "../interface";
+
+export const COLORS: ColorsType[] = [
+  { value: "red", label: "Red", colorCode: "#FF5630" },
+  { value: "green", label: "Green", colorCode: "#36B37E" },
+  { value: "blue", label: "Blue", colorCode: "#0065FF" },
+  { value: "yellow", label: "Yellow", colorCode: "#FFEB3B" },
+  { value: "violet", label: "Violet", colorCode: "#8b00ff" },
+  { value: "orange", label: "Orange", colorCode: "#ff7f00" },
+  { value: "pink", label: "Pink", colorCode: "#ff3399" },
+];
+
+export const WEEK: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
