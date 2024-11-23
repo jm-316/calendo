24c4a4c87bc715c876604846af9f0139922dde86
jm-316 <yoyo3302@gmail.com>
2024-11-23 17:46:14 +0900

    Todopage css 수정

--- src/pages/todos/index.tsx
+++ src/pages/todos/index.tsx
@@ -2,7 +2,7 @@ import Todos from "../../components/todo/Todos";
 
 export default function TodoPage() {
   return (
-    <div className="container">
+    <div className="container p-6">
       <Todos />
     </div>
   );
