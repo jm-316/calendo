78b8b8679606bacd0be62a46ef83423105ab52da
jm-316 <yoyo3302@gmail.com>
2024-11-23 17:45:50 +0900

    COLORS 색상 변경

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,13 +1,13 @@
 import { ColorsType } from "../interface";
 
 export const COLORS: ColorsType[] = [
-  { value: "red", label: "Red", colorCode: "#FF5630" },
+  { value: "red", label: "Red", colorCode: "#fe4545" },
   { value: "green", label: "Green", colorCode: "#36B37E" },
-  { value: "blue", label: "Blue", colorCode: "#0065FF" },
+  { value: "blue", label: "Blue", colorCode: "#528ce4" },
   { value: "yellow", label: "Yellow", colorCode: "#FFEB3B" },
-  { value: "violet", label: "Violet", colorCode: "#8b00ff" },
-  { value: "orange", label: "Orange", colorCode: "#ff7f00" },
-  { value: "pink", label: "Pink", colorCode: "#ff3399" },
+  { value: "violet", label: "Violet", colorCode: "#aa65e5" },
+  { value: "orange", label: "Orange", colorCode: "#fca752" },
+  { value: "pink", label: "Pink", colorCode: "#f7a5ce" },
 ];
 
 export const WEEK: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
