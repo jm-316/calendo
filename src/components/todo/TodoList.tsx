92ce74aab3565861f9aa5cba4c300df3b5716188
jm-316 <yoyo3302@gmail.com>
2024-11-23 17:53:16 +0900

    TodoList 컴포넌트 css 수정

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,14 +1,16 @@
+import { useCount } from "../../hook/useCount";
 import { useTodos } from "../../hook/useTodos";
 
 export default function TodoList() {
-  const { todos, updatedTodo, removeTodo } = useTodos();
+  const { todos, updatedTodo } = useTodos();
+  const { updateCount } = useCount();
 
   const handleCheckboxChange = (id: number, content: string) => {
     const updateTodo = { id, content, completed: true };
 
     updatedTodo.mutate(updateTodo);
     setTimeout(() => {
-      removeTodo.mutate(id);
+      updateCount.mutate(id);
     }, 300);
   };
 
@@ -23,20 +25,20 @@ export default function TodoList() {
     <>
       {todos?.map((todo) => (
         <div
-          className="flex items-center space-x-3 p-4 border-gray-200"
+          className="flex items-center space-x-3 p-2 md:p-3 lg:p-4 border-gray-200"
           key={todo.id}>
           <input
             type="text"
             defaultValue={todo.content}
             onChange={(e) => handleChangeTodos(e, todo.id)}
-            className="todo__input"
+            className="todo__input w-40 md:w-48 h-8 md:h-9 lg:h-10 text-sm md:text-lg"
             placeholder="Enter your to-do item"
           />
           <input
             type="checkbox"
             checked={todo.completed}
             onChange={() => handleCheckboxChange(todo.id, todo.content)}
-            className="h-6 w-6 accent-purple-500 focus:ring-purple-500 rounded border-gray-300"
+            className="h-6 w-6 md:h-4 md:w-4 lg:h-5 lg:w-5 accent-purple-500 focus:ring-purple-500 rounded border-gray-300"
           />
         </div>
       ))}
