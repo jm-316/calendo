6596116c2549b696819799c3bf2ba17381f1e273
jm-316 <yoyo3302@gmail.com>
2024-12-03 17:11:04 +0900

    Month에 보이는 event 위치 수정

--- src/components/calendars/calendar/SingleDayEvent.tsx
+++ src/components/calendars/calendar/SingleDayEvent.tsx
@@ -14,23 +14,25 @@ export default function SingleDayEvent({
   const navigate = useNavigate();
   return (
     <>
-      {events.map((event) => {
+      {events.map((event, index) => {
         const eventDate = format(new Date(event.startDate), "yyyy-MM-dd");
         const calendarDay = format(day, "yyyy-MM-dd");
+        console.log(window.innerHeight);
         if (calendarDay === eventDate) {
+          const eventHeight = window.innerHeight >= 1468 ? 9 : 2;
           const singleEventTop =
             multiDayEventsForDay && multiDayEventsForDay.length > 0
-              ? multiDayEventsForDay.length * 20 + 10
-              : 0;
-
+              ? multiDayEventsForDay.length * 2 +
+                eventHeight +
+                index * eventHeight
+              : index * eventHeight;
           return (
             <div
               key={event.id}
-              className="absolute w-full text-left text-sm"
+              className="relative h-2 lg:h-[22px] lg:w-full lg:pl-2"
               style={{
                 backgroundColor: event.color,
                 top: `${singleEventTop}px`,
-                height: "22px",
               }}
               onClick={(e) => {
                 e.stopPropagation();
@@ -38,7 +40,9 @@ export default function SingleDayEvent({
                   event.id ? `/calendars/${event.id}` : `/calendars/new`
                 );
               }}>
-              {event.title}
+              <div className="hidden lg:block h-1 md:text-sm">
+                {event.title}
+              </div>
             </div>
           );
         }
