5c70a841db127b67224564f0f0a58ec06c5af25e
jm-316 <yoyo3302@gmail.com>
2024-12-05 11:26:40 +0900

    loading 과정 추가

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,9 +1,14 @@
 import { Navigate } from "react-router";
 import { ReactNode } from "react";
+import Loading from "./Loading";
 import { useUser } from "../hook/useUser";
 
 export default function ProtectedRoute({ children }: { children: ReactNode }) {
-  const { user } = useUser();
+  const { user, isLoading } = useUser();
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   if (!user) {
     return <Navigate to="/login" replace />;
